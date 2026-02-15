bb1030c6cf6f46795496b549b4440e2482016c05
b <hongyime@users.noreply.github.com>
2026-02-15 15:02:38 +0800

    llm

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import RoomView from './pages/RoomView';
 import RoomsPage from './pages/RoomsPage';
+import EditSeats from './pages/EditSeats';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/rooms" element={<RoomsPage />} />
       <Route path="/room/:roomId" element={<RoomView />} />
+      <Route path="/edit" element={<EditSeats />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
